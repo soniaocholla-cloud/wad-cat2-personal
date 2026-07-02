84d2fa14d1774f7127e7e9b3094478014dd4c197
soniaocholla-cloud-1 <sonia.ocholla@strathmore.edu>
2026-07-03 02:47:29 +0300

    Add dynamic add and remove order items

--- submission/js/script.js
+++ submission/js/script.js
@@ -49,5 +49,49 @@ menuItems.forEach(function (item) {
     //put the card on the page
     menuGrid.appendChild(card);
 
+});
+
+//build Your Order
+
+let orderInput = document.querySelector("#orderInput");
+let addOrderButton = document.querySelector("#orderForm button");
+let orderItems = document.querySelector("#orderItems");
+
+addOrderButton.addEventListener("click", function(event){
+
+    event.preventDefault();
+
+    if(orderInput.value != ""){
+
+        //create elements
+        let li = document.createElement("li");
+        let button = document.createElement("button");
+
+        //add text
+        li.textContent = orderInput.value;
+        button.textContent = "Delete";
+
+        //delete button
+        button.addEventListener("click", function(){
+
+            li.remove();
+
+        });
+
+        //add button to list item
+        li.appendChild(button);
+
+        //add list item to the list
+        orderItems.appendChild(li);
+
+        //clear textbox
+        orderInput.value = "";
+
+    }
+    else{
+
+        alert("Please type an item.");
+
+    }
 
 });
